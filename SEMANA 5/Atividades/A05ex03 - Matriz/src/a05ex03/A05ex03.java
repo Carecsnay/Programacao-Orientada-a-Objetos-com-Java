7fa79e23d6f48ea24c4128cd4692b6ce4a030e6f
Bruno Benicio de Andrade Lima <carecsnay1@gmail.com>
2022-05-05 07:27:07 -0300

    Updatade Try 2 Damas

--- SEMANA 5/Atividades/A05ex03 - Matriz/src/a05ex03/A05ex03.java
+++ SEMANA 5/Atividades/A05ex03 - Matriz/src/a05ex03/A05ex03.java
@@ -12,103 +12,24 @@ import java.util.Scanner;
 public class A05ex03 {
 
     public static void main(String[] args) {
-        String[][] tabuleiro = new String[8][8];
-        Scanner sc = new Scanner (System.in);      
+        int i, j; //i representa as linhas e j as colunas;
+        int incremento = 1;
         
+        int[][] tabuleiro = new int[8][8]; //Sabendo que um tabuleiro possui 64 casas, criamos uma matriz 8 por 8 (8*8=64)
         
-        //Linha 01
-        
-        String a = "1";
-        tabuleiro[0][0] = a;
-        tabuleiro[0][1] = a;
-        tabuleiro[0][2] = a;
-        tabuleiro[0][3] = a;
-        tabuleiro[0][4] = a;
-        tabuleiro[0][5] = a;
-        tabuleiro[0][6] = a;
-        tabuleiro[0][7] = a;
-        
-        //Linha 02
-        String b = "2";
-        tabuleiro[1][0] = b;
-        tabuleiro[1][1] = b;
-        tabuleiro[1][2] = b;
-        tabuleiro[1][3] = b;
-        tabuleiro[1][4] = b;
-        tabuleiro[1][5] = b;
-        tabuleiro[1][6] = b;
-        tabuleiro[1][7] = b;
-        
-        //Linha 03
-        String c = "3";
-        tabuleiro[2][0] = c;
-        tabuleiro[2][1] = c;
-        tabuleiro[2][2] = c;
-        tabuleiro[2][3] = c;
-        tabuleiro[2][4] = c;
-        tabuleiro[2][5] = c;
-        tabuleiro[2][6] = c;
-        tabuleiro[2][7] = c;
-        
-         //Linha 04
-         String d = "4";
-        tabuleiro[3][0] = d;
-        tabuleiro[3][1] = d;
-        tabuleiro[3][2] = d;
-        tabuleiro[3][3] = d;
-        tabuleiro[3][4] = d;
-        tabuleiro[3][5] = d;
-        tabuleiro[3][6] = d;
-        tabuleiro[3][7] = d;
-        
-         //Linha 05
-         String e = "5";
-        tabuleiro[4][0] = e;
-        tabuleiro[4][1] = e;
-        tabuleiro[4][2] = e;
-        tabuleiro[4][3] = e;
-        tabuleiro[4][4] = e;
-        tabuleiro[4][5] = e;
-        tabuleiro[4][6] = e;
-        tabuleiro[4][7] = e;
-        
-         //Linha 06
-         String f = "6";
-        tabuleiro[5][0] = f;
-        tabuleiro[5][1] = f;
-        tabuleiro[5][2] = f;
-        tabuleiro[5][3] = f;
-        tabuleiro[5][4] = f;
-        tabuleiro[5][5] = f;
-        tabuleiro[5][6] = f;
-        tabuleiro[5][7] = f;
-        
-         //Linha 07
-         String g = "7";
-        tabuleiro[6][0] = g;
-        tabuleiro[6][1] = g;
-        tabuleiro[6][2] = g;
-        tabuleiro[6][3] = g;
-        tabuleiro[6][4] = g;
-        tabuleiro[6][5] = g;
-        tabuleiro[6][6] = g;
-        tabuleiro[6][7] = g;
-        
-         //Linha 08
-         String h = "8";
-        tabuleiro[7][0] = h;
-        tabuleiro[7][1] = h;
-        tabuleiro[7][2] = h;
-        tabuleiro[7][3] = h;
-        tabuleiro[7][4] = h;
-        tabuleiro[7][5] = h;
-        tabuleiro[7][6] = h;
-        tabuleiro[7][7] = h;
-        
-        for (String [] aux : tabuleiro){
-            System.out.println(aux[0]+" "+aux[1]+" "+aux[2]+" "+aux[3]+" "+aux[4]+" "+aux[5]+" "+aux[6]+" "+aux[7]);
+        //preenchendo o tabuleiro
+        for (i=0;i<8;i++){  //linha se inicia em 0 pois os vetores começam com indice 0, ali recebe número 8 porque cada coluna tem 8 linhas e então linha recebe incremento para percorrer todas as 8 posições.
+            for (j=0;j<8;j++){
+                tabuleiro[i][j]=incremento++;
+            } 
         }
         
-        System.out.println("Onde você quer colocar a peça?");
+         //exibindo o tabuleiro
+        for (i=0;i<8;i++){  //linha se inicia em 0 pois os vetores começam com indice 0, ali recebe número 8 porque cada coluna tem 8 linhas e então linha recebe incremento para percorrer todas as 8 posições.
+            for (j=0;j<8;j++){
+                System.out.print("|"+i+"-"+j+"|recebe: "+tabuleiro[i][j]);
+            }
+            System.out.print("\n");
+        }
     }
 }
