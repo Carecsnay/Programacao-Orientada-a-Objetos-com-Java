b96787137e3bb3495885fa4942c7984b397672a9
Bruno B. A. Lima <carecsnay1@gmail.com>
2022-05-04 22:33:32 -0300

    Update Damas

--- SEMANA 5/Atividades/Try/src/a05ex03/A05ex03.java
+++ SEMANA 5/Atividades/Try/src/a05ex03/A05ex03.java
@@ -1,92 +1,114 @@
 package a05ex03;
+/*
 
+    Aluno: Bruno Benicio de Andrade Lima
+    Cidade: Castelo do Piauí - PI
+
+ */
+import java.util.Scanner;
+/*
+   Crie um programa que apresente uma matriz que represente um tabuleiro de damas; O programa deve solicitar a posição inicial da peça (número da linha    e numero da coluna) e a posição final da peça, apresentando o resultado do tabuleiro após o movimento;
+*/
 public class A05ex03 {
 
     public static void main(String[] args) {
         String[][] tabuleiro = new String[8][8];
+        Scanner sc = new Scanner (System.in);      
+        
         
         //Linha 01
-        tabuleiro[0][0] = "1";
-        tabuleiro[0][1] = "1";
-        tabuleiro[0][2] = "1";
-        tabuleiro[0][3] = "1";
-        tabuleiro[0][4] = "1";
-        tabuleiro[0][5] = "1";
-        tabuleiro[0][6] = "1";
-        tabuleiro[0][7] = "1";
+        
+        String a = "1";
+        tabuleiro[0][0] = a;
+        tabuleiro[0][1] = a;
+        tabuleiro[0][2] = a;
+        tabuleiro[0][3] = a;
+        tabuleiro[0][4] = a;
+        tabuleiro[0][5] = a;
+        tabuleiro[0][6] = a;
+        tabuleiro[0][7] = a;
         
         //Linha 02
-        tabuleiro[1][0] = "2";
-        tabuleiro[1][1] = "2";
-        tabuleiro[1][2] = "2";
-        tabuleiro[1][3] = "2";
-        tabuleiro[1][4] = "2";
-        tabuleiro[1][5] = "2";
-        tabuleiro[1][6] = "2";
-        tabuleiro[1][7] = "2";
+        String b = "2";
+        tabuleiro[1][0] = b;
+        tabuleiro[1][1] = b;
+        tabuleiro[1][2] = b;
+        tabuleiro[1][3] = b;
+        tabuleiro[1][4] = b;
+        tabuleiro[1][5] = b;
+        tabuleiro[1][6] = b;
+        tabuleiro[1][7] = b;
         
         //Linha 03
-        tabuleiro[2][0] = "3";
-        tabuleiro[2][1] = "3";
-        tabuleiro[2][2] = "3";
-        tabuleiro[2][3] = "3";
-        tabuleiro[2][4] = "3";
-        tabuleiro[2][5] = "3";
-        tabuleiro[2][6] = "3";
-        tabuleiro[2][7] = "3";
+        String c = "3";
+        tabuleiro[2][0] = c;
+        tabuleiro[2][1] = c;
+        tabuleiro[2][2] = c;
+        tabuleiro[2][3] = c;
+        tabuleiro[2][4] = c;
+        tabuleiro[2][5] = c;
+        tabuleiro[2][6] = c;
+        tabuleiro[2][7] = c;
         
          //Linha 04
-        tabuleiro[3][0] = "4";
-        tabuleiro[3][1] = "4";
-        tabuleiro[3][2] = "4";
-        tabuleiro[3][3] = "4";
-        tabuleiro[3][4] = "4";
-        tabuleiro[3][5] = "4";
-        tabuleiro[3][6] = "4";
-        tabuleiro[3][7] = "4";
+         String d = "4";
+        tabuleiro[3][0] = d;
+        tabuleiro[3][1] = d;
+        tabuleiro[3][2] = d;
+        tabuleiro[3][3] = d;
+        tabuleiro[3][4] = d;
+        tabuleiro[3][5] = d;
+        tabuleiro[3][6] = d;
+        tabuleiro[3][7] = d;
         
          //Linha 05
-        tabuleiro[4][0] = "5";
-        tabuleiro[4][1] = "5";
-        tabuleiro[4][2] = "5";
-        tabuleiro[4][3] = "5";
-        tabuleiro[4][4] = "5";
-        tabuleiro[4][5] = "5";
-        tabuleiro[4][6] = "5";
-        tabuleiro[4][7] = "5";
+         String e = "5";
+        tabuleiro[4][0] = e;
+        tabuleiro[4][1] = e;
+        tabuleiro[4][2] = e;
+        tabuleiro[4][3] = e;
+        tabuleiro[4][4] = e;
+        tabuleiro[4][5] = e;
+        tabuleiro[4][6] = e;
+        tabuleiro[4][7] = e;
         
          //Linha 06
-        tabuleiro[5][0] = "6";
-        tabuleiro[5][1] = "6";
-        tabuleiro[5][2] = "6";
-        tabuleiro[5][3] = "6";
-        tabuleiro[5][4] = "6";
-        tabuleiro[5][5] = "6";
-        tabuleiro[5][6] = "6";
-        tabuleiro[5][7] = "6";
+         String f = "6";
+        tabuleiro[5][0] = f;
+        tabuleiro[5][1] = f;
+        tabuleiro[5][2] = f;
+        tabuleiro[5][3] = f;
+        tabuleiro[5][4] = f;
+        tabuleiro[5][5] = f;
+        tabuleiro[5][6] = f;
+        tabuleiro[5][7] = f;
         
          //Linha 07
-        tabuleiro[6][0] = "7";
-        tabuleiro[6][1] = "7";
-        tabuleiro[6][2] = "7";
-        tabuleiro[6][3] = "7";
-        tabuleiro[6][4] = "7";
-        tabuleiro[6][5] = "7";
-        tabuleiro[6][6] = "7";
-        tabuleiro[6][7] = "7";
+         String g = "7";
+        tabuleiro[6][0] = g;
+        tabuleiro[6][1] = g;
+        tabuleiro[6][2] = g;
+        tabuleiro[6][3] = g;
+        tabuleiro[6][4] = g;
+        tabuleiro[6][5] = g;
+        tabuleiro[6][6] = g;
+        tabuleiro[6][7] = g;
         
          //Linha 08
-        tabuleiro[7][0] = "8";
-        tabuleiro[7][1] = "8";
-        tabuleiro[7][2] = "8";
-        tabuleiro[7][3] = "8";
-        tabuleiro[7][4] = "8";
-        tabuleiro[7][5] = "8";
-        tabuleiro[7][6] = "8";
-        tabuleiro[7][7] = "8";
+         String h = "8";
+        tabuleiro[7][0] = h;
+        tabuleiro[7][1] = h;
+        tabuleiro[7][2] = h;
+        tabuleiro[7][3] = h;
+        tabuleiro[7][4] = h;
+        tabuleiro[7][5] = h;
+        tabuleiro[7][6] = h;
+        tabuleiro[7][7] = h;
         
         for (String [] aux : tabuleiro){
             System.out.println(aux[0]+" "+aux[1]+" "+aux[2]+" "+aux[3]+" "+aux[4]+" "+aux[5]+" "+aux[6]+" "+aux[7]);
         }
+        
+        System.out.println("Onde você quer colocar a peça?");
     }
 }
