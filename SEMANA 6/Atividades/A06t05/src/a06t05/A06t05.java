06ab32c241b60fc0b4ed323aa5dc403d71e73f12
Bruno Benicio de Andrade Lima <carecsnay1@gmail.com>
2022-05-18 12:29:18 -0300

    update tasks

--- SEMANA 6/Atividades/A06t05/src/a06t05/A06t05.java
+++ SEMANA 6/Atividades/A06t05/src/a06t05/A06t05.java
@@ -23,17 +23,70 @@ public class A06t05 {
         System.out.print("Escolha de 1 a 7 para retornar um dia da semana: ");
         int codigo = sc.nextInt();
 
-        System.out.println("Digite a opção de exibição do texto:");
-        System.out.println("["+i+++"] Via prompt");
-        System.out.println("["+ i+++"] Via interface gŕafica");
-        int opcao = sc.nextInt();
-
-        if (opcao == 1) {
-            imprimir(diaSemana(codigo), codigo);
-        }
+        if (codigo > 7) {
+            System.out.println("Opção inválida, escolha de 1 a 7!");
+        } 
         
         else {
-            imprimir(diaSemana(codigo));
+
+            System.out.println("Digite a opção de exibição do texto:");
+            System.out.println("[" + i++ + "] Via prompt");
+            System.out.println("[" + i++ + "] Via interface gŕafica");
+            int opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    imprimir(diaSemana(codigo), codigo);
+                    break;
+                case 2:
+                    imprimir(diaSemana(codigo));
+                    break;
+                default:
+                    System.out.println("Opção Inválida!");
+                    break;
+            }
         }
-    }  
+    }
+
+    private static String diaSemana(int codigoInseridoPeloUsuario) {
+        List<String> dias = new ArrayList<>();
+        dias.add("Domingo");
+        dias.add("Segunda-Feira");
+        dias.add("Terça-Feira");
+        dias.add("Quarta-Feira");
+        dias.add("Quinta-Feira");
+        dias.add("Sexta-Feira");
+        dias.add("Sábado");
+
+        switch (codigoInseridoPeloUsuario) {
+            case 1:
+                return dias.get(0);
+            case 2:
+                return dias.get(1);
+            case 3:
+                return dias.get(2);
+            case 4:
+                return dias.get(3);
+            case 5:
+                return dias.get(4);
+            case 6:
+                return dias.get(5);
+            case 7:
+                return dias.get(6);
+            default:
+                System.out.println("Opção inválida!");
+                break;
+        }
+        return diaSemana(codigoInseridoPeloUsuario);
+    }
+
+    private static String imprimir(String diaSemanaPrompt, int numeroSemana) {
+        System.out.println("O dia escolhido foi: " + diaSemana(numeroSemana));
+        return diaSemana(numeroSemana);
+    }
+
+    private static String imprimir(String diaSemanaInterfaceGrafica) {
+        JOptionPane.showMessageDialog(null, "O dia escolhido foi: " + diaSemanaInterfaceGrafica, "Interface Gráfica", 2);
+        return diaSemanaInterfaceGrafica;
+    }
 }
