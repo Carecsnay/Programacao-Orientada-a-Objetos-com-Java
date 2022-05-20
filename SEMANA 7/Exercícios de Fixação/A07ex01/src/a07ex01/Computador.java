d9d56a0751a1704e808de54aa7978c5bcb4221b7
Bruno B. A. Lima <carecsnay1@gmail.com>
2022-05-19 21:14:07 -0300

    Pratice!

---
+++ SEMANA 7/Exercícios de Fixação/A07ex01/src/a07ex01/Computador.java
@@ -0,0 +1,43 @@
+package a07ex01;
+
+public class Computador {
+
+        private String processador;
+        private String memoria;
+        private String marca;
+        private String monitor;
+
+    public String getProcessador() {
+        return processador;
+    }
+
+    public void setProcessador(String processador) {
+        this.processador = processador;
+    }
+
+    public String getMemoria() {
+        return memoria;
+    }
+
+    public void setMemoria(String memoria) {
+        this.memoria = memoria;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(String monitor) {
+        this.monitor = monitor;
+    }
+
+        
+}
