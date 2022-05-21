f5390463f7ca6cb38295db9a6788a15d61620916
Bruno Benicio de Andrade Lima <carecsnay1@gmail.com>
2022-05-21 07:59:20 -0300

    Task week 7 complete

---
+++ SEMANA 7/Atividades/A07t01/src/a07t01/Fabricante.java
@@ -0,0 +1,21 @@
+package a07t01;
+
+public class Fabricante {
+    private String nome;
+
+    public Fabricante() {
+    }
+
+    public Fabricante(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
