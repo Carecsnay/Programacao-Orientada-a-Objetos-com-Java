1b8d085c679e060a5dded39432630e430071ff93
Bruno B. A. Lima <carecsnay1@gmail.com>
2022-06-10 07:23:43 -0300

    Fix Ex.A09ex05

--- SEMANA 9/Exercícios de Fixação/A09ex05/src/a09ex05/A09ex05.java
+++ SEMANA 9/Exercícios de Fixação/A09ex05/src/a09ex05/A09ex05.java
@@ -17,25 +17,28 @@ public class A09ex05 {
     public static void main(String[] args) {
         try {
             //entrada de dados
-            System.out.println("Digite a sua data de nascimento");
+            System.out.println("Descubra o dia da semana que você nasceu");
+            System.out.print("1º Digite a sua data de nascimento (DD/MM/AAAA): ");
             Scanner scan = new Scanner(System.in);
             String d = scan.next();
-            
+
             //critérios da formatação
-            DateFormat df = DateFormat.getDateInstance();
-            /*
-            Pega o objeto escaneado em String e transforma em um objeto do tipo DATE;
-            A IDE sugere criar uma try catch o código pode gerar uma exceção; 
-            */
-            Date data = df.parse(d); 
-            
-            //Apresentação do dia da semana, de acordo com a documentação a letrinha "E" refere-se ao dia da semana correspondente.;
-            SimpleDateFormat sdf = new SimpleDateFormat("EEEE"); 
-            
-            
-            System.out.print("você nasceu: "+sdf.format(data));
-               
-            
+            SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
+            /* Pega o objeto escaneado em String e transforma em um objeto do tipo DATE;
+            A IDE sugere criar uma try catch o código pode gerar uma exceção; */
+            Date data = df.parse(d);
+
+            //Apresentação do dia da semana, de acordo com a documentação a letrinha "E" refere-se ao dia da semana correspondente, "EEEE" mostra também o dia da semana, porém com o nome completo;
+            SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
+
+            //Validando se o usuário nasceu em UMA "feira" ou em UM "sabado ou domingo".
+            if (sdf.format(data).contains("feira")) {
+                System.out.println("Você nasceu em uma" + sdf.format(data));
+            } else {
+                System.out.println("Você nasceu em um" + sdf.format(data));
+            }
+
         } catch (ParseException ex) {
             Logger.getLogger(A09ex05.class.getName()).log(Level.SEVERE, null, ex);
         }
